b9f95fe7b00fbeb86533e8ddc5075ee65748ef7c
Sukarth Acharya <sukarthacharya@gmail.com>
2026-01-19 05:28:37 +0200

    more signing

--- plugins/withAndroidSigning.js
+++ plugins/withAndroidSigning.js
@@ -15,39 +15,16 @@ function applySigningConfig(buildGradle) {
     return buildGradle;
   }
 
-  // 1. Add 'release' to signingConfigs
-  const releaseConfigBlock = `
-        release {
-            if (project.hasProperty('MYAPP_UPLOAD_STORE_FILE')) {
-                storeFile file(MYAPP_UPLOAD_STORE_FILE)
-                storePassword MYAPP_UPLOAD_STORE_PASSWORD
-                keyAlias MYAPP_UPLOAD_KEY_ALIAS
-                keyPassword MYAPP_UPLOAD_KEY_PASSWORD
-            }
-        }`;
-
-  // Insert release block inside signingConfigs
-  buildGradle = buildGradle.replace(/signingConfigs\s*\{/, `signingConfigs {${releaseConfigBlock}`);
-
-  // 2. Set the signingConfig for release buildType - ALWAYS use release config when properties exist
-  // Match the release buildType block and replace any signingConfig line
-  const releaseBuildTypeMatch =
-    /(buildTypes\s*\{[\s\S]*?release\s*\{[\s\S]*?)(signingConfig\s+signingConfigs\.[a-zA-Z]+)/;
-
-  if (releaseBuildTypeMatch.test(buildGradle)) {
-    buildGradle = buildGradle.replace(releaseBuildTypeMatch, (match, p1, p2) =>
-      match.replace(
-        p2,
-        `signingConfig signingConfigs.release`
-      )
-    );
-  } else {
-    // If no signingConfig line exists in release buildType, add it
-    buildGradle = buildGradle.replace(
-      /(buildTypes\s*\{[\s\S]*?release\s*\{)/,
-      `$1\n            signingConfig signingConfigs.release`
-    );
-  }
+  // 1. Replace the release signingConfigs block to always use properties (no conditional)
+  buildGradle = buildGradle.replace(
+    /release\s*\{[\s\S]*?\}/,
+    `release {
+            storeFile file(MYAPP_UPLOAD_STORE_FILE)
+            storePassword MYAPP_UPLOAD_STORE_PASSWORD
+            keyAlias MYAPP_UPLOAD_KEY_ALIAS
+            keyPassword MYAPP_UPLOAD_KEY_PASSWORD
+        }`
+  );
 
   return buildGradle;
 }
