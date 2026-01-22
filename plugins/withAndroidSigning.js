92786169a92405e1ce3aff307a01cc31176d1a5f
Sukarth Acharya <sukarthacharya@gmail.com>
2026-01-22 19:58:53 +0200

    hmmm

--- plugins/withAndroidSigning.js
+++ plugins/withAndroidSigning.js
@@ -15,37 +15,36 @@ function applySigningConfig(buildGradle) {
     return buildGradle;
   }
 
-  // 1. Add "release" block to signingConfigs so it's available
-  // We use the properties directly (no "if check") because we know CI sets them.
-  const releaseConfigBlock = `
+  // Add signing config that checks for gradle properties
+  const signingConfigCode = `
+    signingConfigs {
+        debug {
+            storeFile file('debug.keystore')
+            storePassword 'android'
+            keyAlias 'androiddebugkey'
+            keyPassword 'android'
+        }
         release {
-            storeFile file(MYAPP_UPLOAD_STORE_FILE)
-            storePassword MYAPP_UPLOAD_STORE_PASSWORD
-            keyAlias MYAPP_UPLOAD_KEY_ALIAS
-            keyPassword MYAPP_UPLOAD_KEY_PASSWORD
-        }`;
+            if (project.hasProperty('MYAPP_UPLOAD_STORE_FILE')) {
+                storeFile file(MYAPP_UPLOAD_STORE_FILE)
+                storePassword MYAPP_UPLOAD_STORE_PASSWORD
+                keyAlias MYAPP_UPLOAD_KEY_ALIAS
+                keyPassword MYAPP_UPLOAD_KEY_PASSWORD
+            }
+        }
+    }`;
 
-  // Insert release block inside signingConfigs { ... }
-  // This adds 'release { ... }' right after 'signingConfigs {'
-  buildGradle = buildGradle.replace(/signingConfigs\s*\{/, `signingConfigs {${releaseConfigBlock}`);
+  // Find android { block and add signingConfigs
+  buildGradle = buildGradle.replace(
+    /(android\s*\{)/,
+    `$1\n${signingConfigCode}\n`
+  );
 
-  // 2. Force the release buildType to use this new signing config
-  // We look for 'buildTypes { ... release { ... }' and replace its signingConfig
-  const releaseBuildTypeMatch =
-    /(buildTypes\s*\{[\s\S]*?release\s*\{[\s\S]*?)(signingConfig\s+signingConfigs\.[a-zA-Z]+)/;
-
-  if (releaseBuildTypeMatch.test(buildGradle)) {
-    // Replace existing signingConfig line
-    buildGradle = buildGradle.replace(releaseBuildTypeMatch, (match, p1, p2) =>
-      match.replace(p2, `signingConfig signingConfigs.release`)
-    );
-  } else {
-    // If no signingConfig line exists in release buildType, insert it
-    buildGradle = buildGradle.replace(
-      /(buildTypes\s*\{[\s\S]*?release\s*\{)/,
-      `$1\n            signingConfig signingConfigs.release`
-    );
-  }
+  // Find buildTypes.release and add signing config reference
+  buildGradle = buildGradle.replace(
+    /(buildTypes\s*\{[\s\S]*?release\s*\{)/,
+    `$1\n            signingConfig signingConfigs.release`
+  );
 
   return buildGradle;
 }
